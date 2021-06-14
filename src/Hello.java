d4b9dbb85986ed6af81b5d68ee8e1a25b80193af
pilsungk-oss <49965974+pilsungk-oss@users.noreply.github.com>
2021-06-14 11:07:41 +0900

    Update Hello.java

--- src/Hello.java
+++ src/Hello.java
@@ -2,6 +2,7 @@
 public class Hello {
 	public static void main(String...strings) {
 		System.out.println("Hello ");
-		System.out.println("World")
+		System.out.println("World");
+		System.out.println("안녕");
 	}
 }
